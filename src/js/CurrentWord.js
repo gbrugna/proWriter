405d20550d1730a357f0f2806a04d429416b7c6a
gabriele <=>
2022-05-19 17:59:49 +0200

    Added comments

--- src/js/CurrentWord.js
+++ src/js/CurrentWord.js
@@ -1,3 +1,15 @@
+/***************************************************************************************
+ * CurrentWord class
+ * Class istantiated into Text class as an attribute. It contains the state of the current word that is being typed.
+ * Data:
+ * - index: the index of the word in the original text
+ * - isCorrect: boolean value that is updated when the word is wrong and when it's not
+ * - currentCharIndex: the currentChar we are in writing (we track it for comparison purposes)
+ * - nErrors: number of errors in the typed word. If it is zero when the word is complete, then the word is correct
+ * - len: length of the current word
+ * - word: the string of the current word
+ ***************************************************************************************/
+
 class CurrentWord {
     constructor(firstWord) {
         this.index = 0;
@@ -8,10 +20,12 @@ class CurrentWord {
         this.word = firstWord;
     }
 
+    //CurrentWord.getHTMLElement(): returns the HTML Element corresponding to the word that is being currently edited
     getHTMLElement() {
         return text.arrayDOM[this.index];
     }
 
+    //CurrentWord.updateColor(): uses the value of CurrentWord.isCorrect in order to update the color of the currentWord (that has just been finalized by pressing space)
     updateColor() {
         let currentWordHTML = this.getHTMLElement();
         if (this.isCorrect)
@@ -20,6 +34,7 @@ class CurrentWord {
             currentWordHTML.classList.add('incorrectWord');
     }
 
+    //CurrentWord.currentChar: returns the last character that has been typed 
     currentChar(){
         return this.word[this.currentCharIndex];
     }
--- src/js/Text.js
+++ src/js/Text.js
@@ -1,3 +1,12 @@
+/***************************************************************************************
+ * Text class
+ * Data:
+ * - original: text as retrieved from the DB 
+ * - originalArray: text splitted into words 
+ * - arrayDOM: array of DOM Elements containing the words 
+ * - currentWord : current word object, initialized with the first word
+ ***************************************************************************************/
+
 class Text {
     constructor() {
         //carica testo dal db
@@ -7,16 +16,21 @@ class Text {
         this.arrayDOM;
     }
 
+    // Text.formatOriginalText(): generates HTML code that will be injected into the originalText DIV. It returns the text as a string.
+    // Appending the text in the div is managed by the Page class, which manages the interactive fields on the page.
     formatOriginalText() {
         let output = '';
         this.originalArray.forEach(word => output += `<span class="word">${word}</span> `);
         return output;
     }
 
+    // Text.readArrayDOM(): loads the array of HTML Elements containing the text's words on the page and saves them in the Text.arrayDOM attribute
+    // basically it creates the array of HTML Elements corresponding to the array of words Text.originalArray.
     readArrayDOM(){
         this.arrayDOM = document.getElementsByClassName('word');
     }
 
+    // Text.nextWord(): changes the state of the CurrentWord object in order to represent the next word.
     nextWord() {
         this.currentWord.index++;
         this.currentWord.currentCharIndex = 0;
@@ -26,9 +40,8 @@ class Text {
         this.currentWord.word = this.originalArray[this.currentWord.index];
     }
 
+    // Text.lastWord(): returns true if the current word is the last word, false otherwise.
     lastWord(){
         return text.currentWord.index == text.originalArray.length-1;
     }
-
-
 }
