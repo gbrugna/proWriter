eee1d62ab57aa87bec406badddadcce243a2684a
redi.vreto@studenti.unitn.it <redi.vreto@studenti.unitn.it>
2022-05-26 21:24:42 +0200

    implemented login and signup backend (no token for the time being)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const port = process.env.PORT;
 
 const textsRouter = require('./routes/texts');
+
+const users = [] //local 'db'
 
 //const swaggerUi = require('swagger-ui-express');
 //const swaggerDocument = require('./swagger.json');
@@ -21,6 +24,60 @@ app.use(express.urlencoded({ extended: true }));
     console.log(req.method + ' ' + req.url)
     next()
 }) */
+
+//signup and login requests
+
+//get the list of all users
+//app.get('/users', (req, res) => {
+    //res.json(users)
+//})
+
+//create a new user
+app.post('/users', async (req, res) => {
+    try {
+        //TODO: check that user doesnt already exist       
+        //checking whether the email is valid
+        const regexExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+        const str = req.body.email
+
+        if(!regexExp.test(str)) {
+            console.log("Not an email")
+        } else {
+            console.log("email ok")
+        }
+
+        console.log(req.body)
+
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+        //TODO: push new user in db
+        const user = { email: req.body.email, password: hashedPassword}
+        users.push(user)   //add user to the db (for the time being hardcoded)
+    } catch {
+        res.status(500).send()
+    }
+    res.sendFile('account_created.html', {root: __dirname + "/public"});
+})
+
+
+//login
+app.post('/users/login', async (req, res) => {
+    console.log(users)
+    const user = users.find(user => user.email === req.body.email)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+
+    try {
+        if (!await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Not allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+    res.sendFile('login_successful.html', {root: __dirname + "/public"});
+})
 
 
 // Delivering static content
