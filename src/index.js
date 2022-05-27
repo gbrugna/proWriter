62d6ed25457e005850746b9d51c4ff6be931b187
redi.vreto@studenti.unitn.it <redi.vreto@studenti.unitn.it>
2022-05-27 16:50:55 +0200

    added jwt lib

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const port = process.env.PORT;
 
