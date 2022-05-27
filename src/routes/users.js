1ab87e310c4e6faa128b70c1a44cdc756f06d150
redi.vreto@studenti.unitn.it <redi.vreto@studenti.unitn.it>
2022-05-27 14:48:17 +0200

    added already existing email check

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,50 +6,50 @@ const User = require('../models/user');
 //create a new user
 router.post('/', async (req, res) => {
     try {
-        //TODO: check that user doesnt already exist       
+        //checking that the user doesn't already exist 
+        const duplicateUser = await User.findOne({ 'email': req.body.email })
+        if (duplicateUser != null) {
+            res.json({login: 'User already exists!'})
+        }
         //checking whether the email is valid
         const regexExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
         const str = req.body.email
 
-        if(!regexExp.test(str)) {
+        if (!regexExp.test(str)) {
             console.log("Not an email")
         } else {
             console.log("email ok")
         }
 
-        
         const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
-      
         let user = new User({
             email: req.body.email,
             password: hashedPassword,
             username: req.body.username,
-            average_wpm : undefined,
-            races_count : 0,
-            precision : undefined
+            average_wpm: undefined,
+            races_count: 0,
+            precision: undefined
         });
-        
+
         insertedUser = await user.save();
         res.location("/api/v1/user/" + insertedUser.id).status(201);
-        res.json({signup : 'successful'});
+        res.json({ signup: 'successful' });
     } catch {
         res.status(500).send()
     }
-
-    
 })
 
 
 //login
 router.post('/login', async (req, res) => {
-    const user = await User.findOne({ 'email' : req.body.email })
-    
+    const user = await User.findOne({ 'email': req.body.email })
+
     if (user == null) {
         return res.status(400).send('Cannot find user') //C'è un motivo particolare per 400 e non 404?
     }
- 
+
     try {
         if (!await bcrypt.compare(req.body.password, user.password)) {
             res.send('Not allowed')
@@ -57,15 +57,15 @@ router.post('/login', async (req, res) => {
     } catch {
         res.status(500).send()
     }
-    res.json({login : 'successful'});
+    res.json({ login: 'successful' });
 })
 
 //signup and login requests
 
 //get the list of all users
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
     let users = await User.find({}).exec();
-    
+
     res.json(users);
 })
 
