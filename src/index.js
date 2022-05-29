7ca811fcb21c33551fc7b1ea56b5fd3e0dc691d0
redi.vreto@studenti.unitn.it <redi.vreto@studenti.unitn.it>
2022-05-29 20:14:04 +0200

    handling log-in for already logged in users

--- src/index.js
+++ src/index.js
@@ -30,24 +30,32 @@ app.use(cookieParser())
 // Delivering static content
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    res.sendFile('index.html', {root: __dirname + "/public"});
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname + "/public" });
 })
 
-app.get('/game', (req,res)=>{
-    res.sendFile('game.html', {root: __dirname + "/public"});
+app.get('/game', (req, res) => {
+    res.sendFile('game.html', { root: __dirname + "/public" });
 })
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
+    //checking whether the user has already logged in
     var token = req.cookies.auth
     if (token != null) {
-        res.sendFile('logout.html', {root: __dirname + "/public"});
+        //making sure that the token contained in the cookie was not tampered with
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
+            if (err) return res.status(403).json({message: 'invalid token'})
+            req.data = decodedData  //at this point we know for sure that the information contained in data is valid
+        })
+        var currentEmail = req.data.email
+        res.json({login: 'already logged in as ' + currentEmail})
+        return;
     }
-    res.sendFile('login.html', {root: __dirname + "/public"});
+    res.sendFile('login.html', { root: __dirname + "/public" });
 })
 
-app.get('/account', (req, res)=>{
-    res.sendFile('profile.html', {root: __dirname + "/public"});
+app.get('/account', (req, res) => {
+    res.sendFile('profile.html', { root: __dirname + "/public" });
 })
 
 //Resources routing
@@ -55,14 +63,26 @@ app.use('/api/v1/texts', textsRouter);
 app.use('/api/v1/user', userRouter);
 
 //TMP
-app.post('/api/v1/user/:id/score', (req,res)=>{
-    console.log(req.params.id, req.body.wpm, req.body.precision);
-    res.json({success : true});
+app.post('/api/v1/user/:id/score', (req, res) => {
+    //extracting the token from the cookie
+    var token = req.cookies.auth
+    if (token == null) return res.sendStatus(401).json({message: 'no token provided'})
+    console.log("token provided by the user")
+
+    //verifying that the token was not tampered with
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
+        if (err) return res.status(403).json({message: 'invalid token'})
+        req.data = decodedData  //at this point we know for sure that the information contained in data is valid
+    })
+
+    //TODO: write in the DB the new score accordingly, the email, which is always unique is in req.data.email
+    //console.log(req.params.id, req.body.wpm, req.body.precision);
+    res.json({ success: true });
 })
 
 //Connection to database
-app.locals.db = mongoose.connect(process.env.DB_URL).then(()=>{
+app.locals.db = mongoose.connect(process.env.DB_URL).then(() => {
     console.log("Connected to database");
-    app.listen(port, ()=>{console.log(`Listening on port ${port}`)});
+    app.listen(port, () => { console.log(`Listening on port ${port}`) });
 })
-.catch((e)=>{console.log("Error in database connection" , process.env.DB_URL)});
+    .catch((e) => { console.log("Error in database connection", process.env.DB_URL) });
