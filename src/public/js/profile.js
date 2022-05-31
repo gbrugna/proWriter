f4292460d400f0f3afdedb0faf66967d6ca61142
gabriele <=>
2022-05-31 22:51:32 +0200

    added support for error messages

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -23,6 +23,9 @@ function text(action) {
     if (action == "add") {
         document.getElementById("add-text-container").classList.remove("invisible");
         document.getElementById("tab-container").classList.add("invisible");
+        document.getElementById("addBtn").classList.remove("invisible");
+        document.getElementById("text-to-add").value = '';
+        document.getElementById("outcomeAddText").classList.add("invisible");
     } else if (action == "remove") {
         document.getElementById("remove-text-container").classList.remove("invisible");
         document.getElementById("tab-container").classList.add("invisible");
@@ -47,8 +50,11 @@ async function toServer(action) {
         //check if it's administrator before send request server-side
         if (action == "add") {
             let textToAdd = document.getElementById("text-to-add").value;
-            console.log(textToAdd);
 
+            if(textToAdd.length == 0){
+                displayOutcome('Impossibile inserire un testo vuoto!');
+                return;
+            }
             const response = await fetch('/api/v1/admin/addText', {
                 method: 'POST',
                 headers: {
@@ -56,23 +62,29 @@ async function toServer(action) {
                 },
                 body: JSON.stringify({content: textToAdd})
             }).catch(error => console.error(error));
-        
             const body = await response.json();
-            if(body.state == 'success') {
-                console.log('success!');
-                //TODO: tell admin the outcome of the insertion via the UI
-            } else {
-                console.log('fail!');
-                //TODO: tell admin the outcome of the insertion via the UI
-            }
-
+            if(body.state == 'success'){
+                displayOutcome('Testo aggiunto con successo', false);
+                document.getElementById('addBtn').classList.add('invisible');
+            }else
+                displayOutcome('Il testo non è stato aggiunto');
         } else if (action == "remove") {
             let textToRemove = document.getElementById("text-to-remove").value;
             console.log(textToRemove);
             //remove text from server -> check if it's an administrator before server-side
         }
     }
-    hide(action);
+    //hide(action);
+}
+
+function displayOutcome(message, error = true){
+    let messageBox = document.getElementById('outcomeAddText');
+    messageBox.innerText = message;
+    messageBox.classList.remove('invisible');
+    if(!error)
+        messageBox.classList.add('success');
+    else
+    messageBox.classList.remove('success');
 }
 
 async function logout(){
