95e862b5ba6d9936545c5314dfdf563a57f3dcfb
redi.vreto@studenti.unitn.it <redi.vreto@studenti.unitn.it>
2022-06-02 15:43:55 +0200

    added middleware function to verify whether a user is an admin or not

--- src/routes/texts.js
+++ src/routes/texts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Text = require('../models/text');
+const Admin = require('../models/user');
 
 router.get('/', async (req, res)=>{
     let texts = await Text.find({}).exec();
@@ -52,5 +53,22 @@ router.delete('/:id', async (req, res)=>{
     await text.deleteOne()
     res.status(204).send()
 });
+
+async function isAdmin(req, res, next) {
+    const token = req.cookies.auth;
+    if (token == null)
+        return res.status(200).json({ state: 'false' });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
+        if (err) return res.status(200).json({ state: 'false' });
+        req.data = decodedData;
+    });
+
+    let administrator = await Admin.findOne({ email: req.data.email });
+    if (administrator == null || administrator.admin != 'true')
+        return res.status(200).json({ state: 'false' });
+
+    next();
+}
 
 module.exports = router;
