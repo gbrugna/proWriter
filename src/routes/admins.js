a394cacd4e2cce65fdda46fff45827e1cea1b777
redi.vreto@studenti.unitn.it <redi.vreto@studenti.unitn.it>
2022-06-02 15:44:03 +0200

    added middleware function to verify whether a user is an admin or not

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -7,42 +7,12 @@ const Admin = require('../models/user');
 const Text = require('../models/text')
 
 //check if the user is an admin
-router.post('/verify', async (req, res) => {
-    const token = req.cookies.auth;
-    if (token == null) {
-        return res.status(200).json({ state: 'false' });
-    }
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
-        if (err) return res.status(200).json({ state: 'false' });
-        req.data = decodedData  //at this point we know for sure that the information contained in data is valid
-    })
-
-    let administrator = await Admin.findOne({ email: req.data.email });
-    if (administrator == null) {
-        return res.status(200).json({ state: 'false' });
-    } else {
-        return res.status(200).json({ state: administrator.admin == 'true' });
-    }
+router.post('/verify', isAdmin, async (req, res) => {
+    return res.status(200).json({ state: true });
 });
 
 //add text to db
-router.post('/addText', async (req, res) => {
-    const token = req.cookies.auth;
-    if (token == null) {
-        return res.status(200).json({ state: 'false' });
-    }
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
-        if (err) return res.status(200).json({ state: 'false' });
-        req.data = decodedData  //at this point we know for sure that the information contained in data is valid
-    })
-
-    let administrator = await Admin.findOne({ email: req.data.email });
-    if (administrator == null) {
-        return res.status(200).json({ state: 'false' });
-    }
-
+router.post('/addText', isAdmin, async (req, res) => {
     var text = new Text({
         content: req.body.content
     });
@@ -55,5 +25,22 @@ router.post('/addText', async (req, res) => {
     });
     return res.status(200).json({ state: 'success' });
 })
+
+async function isAdmin(req, res, next) {
+    const token = req.cookies.auth;
+    if (token == null)
+        return res.status(200).json({ state: 'false' });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
+        if (err) return res.status(200).json({ state: 'false' });
+        req.data = decodedData;
+    });
+
+    let administrator = await Admin.findOne({ email: req.data.email });
+    if (administrator == null || administrator.admin != 'true')
+        return res.status(200).json({ state: 'false' });
+
+    next();
+}
 
 module.exports = router;
