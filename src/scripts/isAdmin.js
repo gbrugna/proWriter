db9a48d92133050568167481e7d7cec66ab8b644
gabriele <=>
2022-06-03 15:28:52 +0200

    /api/v1/user APIs documented

--- src/scripts/isAdmin.js
+++ src/scripts/isAdmin.js
@@ -3,18 +3,9 @@ const User = require('../models/user');
 
 
 async function isAdmin(req, res, next) {
-    const token = req.cookies.auth;
-    if (token == null)
-        return res.status(200).json({ state: 'false' });
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decodedData) => {
-        if (err) return res.status(200).json({ state: 'false' });
-        req.data = decodedData;
-    });
-
     let administrator = await User.findOne({ email: req.data.email });
     if (administrator == null || administrator.admin != 'true')
-        return res.status(200).json({ state: 'false' });
+        return res.status(403).json({ state: 'false' });
 
     next();
 }
