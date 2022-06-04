d8a24633f2289f44a7d445e97a87f62819f319b8
Saverio Morelli <saverio.morelli@outlook.it>
2022-06-04 23:56:35 +0200

    Improve UI for milliseconds and fixed "undefined" views for timer

--- src/public/js/game/Counter.js
+++ src/public/js/game/Counter.js
@@ -23,13 +23,23 @@ class Counter {
     getFormattedTime(time) {
         //return the time formatted as "X sec" or "X min Y sec" or "X h Y min Z sec"
         let timeToReturn = "0 ms";
-        if (time < 100) timeToReturn = time + "ms"
-        else {
-            let timeInSeconds = parseInt(time / 100);
-            let milliseconds = time - (timeInSeconds * 100);
-            if (timeInSeconds < 60) timeToReturn = timeInSeconds + " sec " + milliseconds + " ms";
-            else if (timeInSeconds < 60 * 60) timeToReturn = (parseInt(timeInSeconds / 60)) + " min " + (timeInSeconds % 60) + " sec " + milliseconds + " ms";
-            else if (timeInSeconds < 60 * 60 * 60) timeToReturn = (parseInt(timeInSeconds / (60 * 60))) + " h " + (parseInt(timeInSeconds / (60) - 60 * parseInt(timeInSeconds / (60 * 60)))) + " min " + (timeInSeconds % 60) + " sec " + milliseconds + " ms";
+        let timeToUse = time * 10;
+        if (timeToUse < 1000) {
+            let milliseconds = timeToUse;
+            if (milliseconds < 1) milliseconds = 1;
+            if (milliseconds.toString().length <= 2) milliseconds = milliseconds * 10;
+            if (milliseconds.toString().length <= 3) milliseconds = milliseconds * 10;
+            let millisecondsToUse = "<span>" + milliseconds.toString()[0] + "</span><span class='smaller-timer-ms'>" + milliseconds.toString()[1] + "" + milliseconds.toString()[2] + "</span>";
+            timeToReturn = millisecondsToUse + " ms";
+        } else {
+            let timeInSeconds = parseInt(timeToUse / 1000);
+            let milliseconds = timeToUse - (timeInSeconds * 1000);
+            if (milliseconds.toString().length <= 2) milliseconds = milliseconds * 10;
+            if (milliseconds.toString().length <= 3) milliseconds = milliseconds * 10;
+            let millisecondsToUse = "<span>" + milliseconds.toString()[0] + "</span><span class='smaller-timer-ms'>" + milliseconds.toString()[1] + "" + milliseconds.toString()[2] + "</span>";
+            if (timeInSeconds < 60) timeToReturn = timeInSeconds + " sec " + millisecondsToUse + " ms";
+            else if (timeInSeconds < 60 * 60) timeToReturn = (parseInt(timeInSeconds / 60)) + " min " + (timeInSeconds % 60) + " sec " + millisecondsToUse + " ms";
+            else if (timeInSeconds < 60 * 60 * 60) timeToReturn = (parseInt(timeInSeconds / (60 * 60))) + " h " + (parseInt(timeInSeconds / (60) - 60 * parseInt(timeInSeconds / (60 * 60)))) + " min " + (timeInSeconds % 60) + " sec " + millisecondsToUse + " ms";
         }
         return timeToReturn;
     }
